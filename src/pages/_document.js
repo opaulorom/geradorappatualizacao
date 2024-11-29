283f19c7f73058de73d574dd6d2e2023993d141f
opaulorom <produtoart.venda@gmail.com>
2024-11-29 10:12:09 -0300

    Trocando o Fivecom

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,12 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Favicon */}
+        <link rel="icon" href="/fraicon.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
